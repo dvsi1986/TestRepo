3f583a24ca70e55cc70f81e618822240b56bb82b
dvsi1986 <dvsi1986@gmail.com>
2017-07-21 14:54:16 -0500

    Initial Check In of Sales Tool

---
+++ SalesTool/src/com/test/git/repo/Mail.java
@@ -0,0 +1,17 @@
+package com.test.git.repo;
+
+public class Mail {
+
+	public static void main(String[] args) {
+		SalesData s = new SalesData();
+
+		dispayGreetings();
+		System.out.println("-----------");
+	}
+
+	private static void dispayGreetings() {
+		System.out.println("HELLO	 HAPPY SALES PEOPLE");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
---
+++ SalesTool/src/com/test/git/repo/SalesData.java
@@ -0,0 +1,8 @@
+package com.test.git.repo;
+
+public class SalesData {
+	public void displayData() {
+		System.out.println("To-do..");
+	}
+
+}
