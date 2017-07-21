87b5406dd4e43d3a1a37da7651acf950c06b20a8
dvsi1986 <dvsi1986@gmail.com>
2017-07-21 16:40:19 -0500

    patchup branch changes are done

--- SalesTool/src/com/test/git/repo/Main.java
+++ SalesTool/src/com/test/git/repo/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is Main Class of send check in ..");
 		System.out.println("Here onwords ptchup branch will work for my task..");
+		System.out.println("this is ptchup branch ");
 	}
 
 }
