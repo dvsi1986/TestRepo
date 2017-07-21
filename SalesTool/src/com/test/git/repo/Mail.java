d5ab1e6d673e7fb37a94424a2d7cce35647b7a15
dvsi1986 <dvsi1986@gmail.com>
2017-07-21 16:18:24 -0500

    DownStream Done! Updated something..

--- SalesTool/src/com/test/git/repo/Mail.java
+++ SalesTool/src/com/test/git/repo/Mail.java
@@ -14,6 +14,8 @@ public class Mail {
 		System.out.println("THIS APP SHOWS SALES DATA");
 		System.out.println("**************************");
 		System.out.println("I would like to add more code !!");
+		System.out.println("################################");
+		System.out.println("After Checkout a project from git..");
 	}
 
 }
