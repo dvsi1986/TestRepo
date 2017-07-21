ced256a64a7d94ce8a564418809c6a0ecac3c008
dvsi1986 <dvsi1986@gmail.com>
2017-07-21 18:11:27 -0500

    Update ceotask.java

--- SalesTool/src/com/test/git/repo/ceotask.java
+++ SalesTool/src/com/test/git/repo/ceotask.java
@@ -3,7 +3,7 @@ package com.test.git.repo;
 public class ceotask {
 
 	public static void main(String[] args) {		
-		System.out.println("i value - " + i);		
+		System.out.println("i value - ");		
 	}
 
 }
