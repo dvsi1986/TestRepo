9afd28aa9f441cd8ba68ee8b95a5263da05f2a26
dvsi1986 <dvsi1986@gmail.com>
2017-07-21 19:18:22 -0500

    Update Main.java

--- SalesTool/src/com/test/git/repo/Main.java
+++ SalesTool/src/com/test/git/repo/Main.java
@@ -3,9 +3,11 @@ package com.test.git.repo;
 public class Main {
 
 	public static void main(String[] args) {
-		String s[]={"siva","Krishna","Sai"};
+		String s[]={"siva","Krishna","Sai","Hare","Ram","HeyRam"};
 		for(String ss:s){
 			System.out.println("Name-> "+ss);
+			System.out.println("You're so great!!");
+			
 		}
 	}
 
