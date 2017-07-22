b5df175174b2920190e0effeba778b0c0ff4ffaf
dvsi1986 <dvsi1986@gmail.com>
2017-07-21 19:16:02 -0500

    d

--- SalesTool/src/com/test/git/repo/ceotask.java
+++ SalesTool/src/com/test/git/repo/ceotask.java
@@ -5,7 +5,6 @@ public class ceotask {
 	public static void main(String[] args) {
 		for (int i = 10; i >= 1; i--) {
 			System.out.println("i value - " + i*20);
-			
 		}
 		System.out.println("i value - ");
 	
